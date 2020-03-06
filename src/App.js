e06c506b0a35b9f46904919454b689a5719813d1
Tayfun Sur <tayfunsur@gmail.com>
2020-03-06 23:49:36 +0300

    header is added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Started the project
-      </header>
+      
+        <Header />
+     
     </div>
   );
 }
