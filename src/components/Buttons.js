4fd5331bf474a7e7e8a294cf7de793c63007eddd
Tayfun Sur <tayfunsur@gmail.com>
2020-03-07 09:17:08 +0300

    lifting state from buttons to button is done

--- src/components/Buttons.js
+++ src/components/Buttons.js
@@ -5,6 +5,8 @@ class Button extends React.Component {
   constructor(props) {
     super(props);
     this.handleMenuClick = this.handleMenuClick.bind(this);
+    this.handleContactClick = this.handleContactClick.bind(this);
+    this.handleHomeClick = this.handleHomeClick.bind(this);
     this.state = {
       isMenuClicked: false,
       isHomeClicked: false,
@@ -16,22 +18,42 @@ class Button extends React.Component {
     this.setState({isMenuClicked: true});
     console.log('menüye tıklandı')
   }
+  handleContactClick() {
+    this.setState({isContactClicked: true});
+    console.log('contacta tıklandı')
+  }
+  handleHomeClick() {
+    this.setState({isHomeClicked: true});
+    console.log('homa tıklandı')
+  }
 
   render() {
     return (
       <div className='tab-bar'>
-        <Contacts />
+        <Contact onContactClick={this.handleContactClick}/>
         <Menu onMenuClick={this.handleMenuClick}/>
-        <Home />      
+        <Home onHomeClick={this.handleHomeClick}/>      
       </div>
     )
   }
 }
 
-function Home(){
-  return(   
-    <button className='btnNav'>Home</button>    
-  )
+class Home extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {isOpened: false};
+    this.homeClickHandler = this.homeClickHandler.bind(this);
+  }
+  
+  homeClickHandler() {
+    this.props.onHomeClick();    
+  }
+  
+  render() {
+    return(
+      <button className='btnNav' onClick={this.homeClickHandler}>Home</button>    
+    )
+  }    
 }
 class Menu extends React.Component {
   constructor(props){
@@ -50,10 +72,22 @@ class Menu extends React.Component {
     )
   }    
 }
-function Contacts(){
-  return(  
-    <button className='btnNav' >Contact</button> 
-  )
+class Contact extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {isOpened: false};
+    this.contactClickHandler = this.contactClickHandler.bind(this);
+  }
+  
+  contactClickHandler() {
+    this.props.onContactClick();    
+  }
+  
+  render() {
+    return(
+      <button className='btnNav' onClick={this.contactClickHandler}>Contact</button>    
+    )
+  }    
 }
 
 
