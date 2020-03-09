3e58dae49a538d285f75d65165a247407fbb6bdc
Tayfun Sur <tayfunsur@gmail.com>
2020-03-09 17:26:04 +0300

    Updates

---
+++ precache-manifest.1e6eba9e115548b910743097def9733a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8573c80271b898114767f733790fabb8",
+    "url": "/react-restaurant/index.html"
+  },
+  {
+    "revision": "8c5b2b4d0dff3a3be276",
+    "url": "/react-restaurant/static/css/main.2dd2042a.chunk.css"
+  },
+  {
+    "revision": "f38e1076795b86aea468",
+    "url": "/react-restaurant/static/js/2.8e172bc2.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/react-restaurant/static/js/2.8e172bc2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c5b2b4d0dff3a3be276",
+    "url": "/react-restaurant/static/js/main.711afb76.chunk.js"
+  },
+  {
+    "revision": "d73ad7513b5a1fd9d11f",
+    "url": "/react-restaurant/static/js/runtime-main.3f9e8686.js"
+  },
+  {
+    "revision": "c163500aee56f5201e1187c42e19471c",
+    "url": "/react-restaurant/static/media/background.c163500a.jpg"
+  },
+  {
+    "revision": "27fb918fc52308bdee77a026ad131135",
+    "url": "/react-restaurant/static/media/menu.27fb918f.jpg"
+  }
+]);
